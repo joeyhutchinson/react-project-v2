72fc155a2d494f8a5bf6a2ce400b842bbeb0a21e
Joey Hutchinson <jhutchinson@petstock.com.au>
2019-11-08 11:18:01 +1100

    Horizontal scroll disabled via styling change in slider.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Footer = (props) => {
-  return <footer>{/* <h2>footer</h2> */}</footer>;
+  return <footer style={{textAlign: "center", color: "#fff"}}>
+    <p style={{fontSize: "0.8rem"}}>Website designed and created with ReactJS by Joey. All imagery provided by © NASA/JPL/University of Arizona.</p></footer>;
 }
 export default Footer;
